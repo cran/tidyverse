bc85c68edd0a952e0fdcba581090760a5f1b96c2
Hadley Wickham <hadley@rstudio.com>
2019-11-21 04:30:02 +0000

    version 1.3.0

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -3,6 +3,7 @@
 
 # Suppress R CMD check note
 #' @importFrom broom tidy
+#' @importFrom dbplyr db_collect
 #' @importFrom forcats fct_c
 #' @importFrom ggplot2 ggplot
 #' @importFrom haven read_sas
@@ -11,8 +12,11 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom lubridate ymd
 #' @importFrom modelr add_predictions
+#' @importFrom pillar pillar
 #' @importFrom readr read_csv
 #' @importFrom readxl read_excel
+#' @importFrom reprex reprex
+#' @importFrom rlang qq_show
 #' @importFrom rvest html_node
 #' @importFrom stringr str_c
 #' @importFrom tidyr spread
@@ -20,9 +24,9 @@
 NULL
 
 
-release_questions <- function() {
+release_bullets <- function() {
   c(
-    "Have you run `usethis::use_tidy_versions(TRUE)`?",
-    "Have you tested with RStudio 1.0?"
+    '`usethis::use_latest_dependencies(TRUE, "CRAN")`',
+    '`tidyverse_dependency_dissuade()`'
   )
 }
--- R/update.R
+++ R/update.R
@@ -4,16 +4,15 @@
 #' dependencies) are up-to-date, and will install after an interactive
 #' confirmation.
 #'
-#' @param recursive If \code{TRUE}, will also check all dependencies of
-#'   tidyverse packages.
+#' @inheritParams tidyverse_deps
 #' @export
 #' @examples
 #' \dontrun{
 #' tidyverse_update()
 #' }
-tidyverse_update <- function(recursive = FALSE) {
+tidyverse_update <- function(recursive = FALSE, repos = getOption("repos")) {
 
-  deps <- tidyverse_deps(recursive)
+  deps <- tidyverse_deps(recursive, repos)
   behind <- dplyr::filter(deps, behind)
 
   if (nrow(behind) == 0) {
@@ -34,13 +33,44 @@ tidyverse_update <- function(recursive = FALSE) {
   invisible()
 }
 
+#' Get a situation report on the tidyverse
+#'
+#' This function gives a quick overview of the versions of R and RStudio as
+#' well as all tidyverse packages. It's primarily designed to help you get
+#' a quick idea of what's going on when you're helping someone else debug
+#' a problem.
+#'
+#' @export
+tidyverse_sitrep <- function() {
+  cli::cat_rule("R & RStudio")
+  if (rstudioapi::isAvailable()) {
+    cli::cat_bullet("RStudio: ", rstudioapi::getVersion())
+  }
+  cli::cat_bullet("R: ", getRversion())
+
+  deps <- tidyverse_deps()
+  package_pad <- format(deps$package)
+  packages <- ifelse(
+    deps$behind,
+    paste0(cli::col_yellow(cli::style_bold(package_pad)), " (", deps$local, " < ", deps$cran, ")"),
+    paste0(package_pad, " (", deps$cran, ")")
+  )
+
+  cli::cat_rule("Core packages")
+  cli::cat_bullet(packages[deps$package %in% core])
+  cli::cat_rule("Non-core packages")
+  cli::cat_bullet(packages[!deps$package %in% core])
+}
+
 #' List all tidyverse dependencies
 #'
 #' @param recursive If \code{TRUE}, will also list all dependencies of
 #'   tidyverse packages.
+#' @param repos The repositories to use to check for updates.
+#'   Defaults to \code{getOptions("repos")}.
 #' @export
-tidyverse_deps <- function(recursive = FALSE) {
-  pkgs <- utils::available.packages()
+tidyverse_deps <- function(recursive = FALSE, repos = getOption("repos")) {
+  pkgs <- utils::available.packages(repos = repos)
   deps <- tools::package_dependencies("tidyverse", pkgs, recursive = recursive)
 
   pkg_deps <- unique(sort(unlist(deps)))
@@ -52,8 +82,11 @@ tidyverse_deps <- function(recursive = FALSE) {
   )
   pkg_deps <- setdiff(pkg_deps, base_pkgs)
 
+  tool_pkgs <- c("cli", "crayon", "rstudioapi")
+  pkg_deps <- setdiff(pkg_deps, tool_pkgs)
+
   cran_version <- lapply(pkgs[pkg_deps, "Version"], base::package_version)
-  local_version <- lapply(pkg_deps, utils::packageVersion)
+  local_version <- lapply(pkg_deps, packageVersion)
 
   behind <- purrr::map2_lgl(cran_version, local_version, `>`)
 
@@ -63,4 +96,12 @@ tidyverse_deps <- function(recursive = FALSE) {
     local = local_version %>% purrr::map_chr(as.character),
     behind = behind
   )
+}
+
+packageVersion <- function(pkg) {
+  if (rlang::is_installed(pkg)) {
+    utils::packageVersion(pkg)
+  } else {
+    0
+  }
 }
